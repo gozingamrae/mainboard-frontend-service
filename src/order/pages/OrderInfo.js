dea0066253275decce49a435b9e8ef68743a3bcd
gomdoricha <chayoungjun5781@gmail.com>
2022-09-16 18:04:48 +0900

    Refactor OrderInfo페이지 수정

--- src/order/pages/OrderInfo.js
+++ src/order/pages/OrderInfo.js
@@ -2,10 +2,10 @@ import '../css/order-info-style.css';
 function OrderInfo(){
 
     const boardGameName = "부루마블";
-    const LendPrice = "4000"
+    const LoanPrice = "4000"
     const deliveredPrice ="2500";
     const boardGameCode="1234567890";
-    const details="상품 상세 정보";
+    const details=" . . . 상세정보 . . . ";
 
 
     return(
@@ -14,6 +14,10 @@ function OrderInfo(){
             
             <img className="image" src="img/burumavel.jpg" alt="사진출력오류"/>
             <p>보드게임명 : {`${boardGameName}`}</p>
+            <p>대여료 : {`${LoanPrice}`}</p>
+            <p>배송비 : {`${deliveredPrice}`}</p>
+            <p>보드게임 코드 : {`${boardGameCode}`}</p>
+            <p>제품 상세 정보 : {`${details}`}</p>
         </div>
     );
 }
