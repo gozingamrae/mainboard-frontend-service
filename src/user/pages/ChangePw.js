b0b5fe5f393219a5c477f942930cc73a35fd17de
LeeYuri <56033943+lyr426@users.noreply.github.com>
2022-09-18 22:45:12 +0900

    Add 비밀번호 변경 페이지

---
+++ src/user/pages/ChangePw.js
@@ -0,0 +1,25 @@
+import "../style.css";
+
+function ChangePw(){
+
+    return (
+        <div className="find">
+
+            <div className="find-title"><p>비밀번호 변경하기</p></div>
+
+            <form className="find-form find-pwd">
+                <div className="find-input">
+                <input type="password" name="password" placeholder="새 비밀번호"></input>
+                </div>
+                <div className="find-input">
+                <input type="password" name="password2" placeholder="새 비밀번호 확인"></input>
+                </div>
+                <div className="agreement-btns input-submit">
+                    <input type="submit" value="다음"/> 
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default ChangePw;
