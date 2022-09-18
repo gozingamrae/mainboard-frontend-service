0bef01e97766000db8c93b63b4da6fe19c9176d9
gomdoricha <chayoungjun5781@gmail.com>
2022-09-18 14:24:11 +0900

    Add order파일 버튼으로 페이지 이동 기능 추가

--- src/order/pages/OrderInfo.js
+++ src/order/pages/OrderInfo.js
@@ -2,22 +2,49 @@ import '../css/order-info-style.css';
 function OrderInfo(){
 
     const boardGameName = "부루마블";
-    const LoanPrice = "4000"
+    const benefit =5;
+    const loanPrice = 4000;
     const deliveredPrice ="2500";
     const boardGameCode="1234567890";
     const details=" . . . 상세정보 . . . ";
+    const points = loanPrice*(benefit/100);
 
+    function onClickHandler1(e){
+        window.location.href= '/shoppingcart';
+    };  
+    function onClickHandler2(e){
+        window.location.href= '/wishlist';
+    };  
+    function onClickHandler3(e){
+        window.location.href= '/order-info';
+    };  
 
     return(
-        <div>
+        <div className='contentBox'>
+
             <h1>+++ HEADER +++</h1>
-            
+            <hr/>
+
             <img className="image" src="img/burumavel.jpg" alt="사진출력오류"/>
-            <p>보드게임명 : {`${boardGameName}`}</p>
-            <p>대여료 : {`${LoanPrice}`}</p>
+
+            <div className='content'>
+            <p className='boardGameName' >{`${boardGameName}`}</p>
+            <hr/>
+            <p className='loanPrice'>대여료 : {`${loanPrice}`}</p>
+            <p>대여혜택 : {`적립 포인트 ${benefit}% (${points}원)`}</p>
             <p>배송비 : {`${deliveredPrice}`}</p>
             <p>보드게임 코드 : {`${boardGameCode}`}</p>
             <p>제품 상세 정보 : {`${details}`}</p>
+            <hr/>
+            <button  className='button' onClick={onClickHandler1}> 장바구니 </button>
+            <button className='button' onClick={onClickHandler2}>찜하기</button>
+            <button className='buyButton'onClick={onClickHandler3}>바로 구매</button>
+            </div>
+
+
+            
+            <h1 className='footer'>+++ Footer +++</h1>
+             
         </div>
     );
 }
