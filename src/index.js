cd0b2ec17a7647e4d8dc247ff532973b3c1bb1b8
DonghwooCho <whehdgn1001@kookmin.ac.kr>
2022-09-19 12:53:28 +0900

    Fix 리덕스 파일 에러 해결
    리덕스 파일 에러 해결했습니다.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./fonts/font.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import Store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+  <Provider store={Store}>
     <App />
   </Provider>
 );
