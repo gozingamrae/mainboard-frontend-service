a4b1e85c6cfc87c1521ee57e1aca9d758975fdcc
gomdoricha <chayoungjun5781@gmail.com>
2022-09-20 14:45:41 +0900

    Add pull을 위한 commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import BoardgameInfo from './order/pages/Order';
+import BoardgameInfo from './order/pages/boardgameInfo.js';
+import OrderInfo from './order/pages/order-info.js';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/boardgame/list/:1" element = { <Order/> } />
+      <Route path="/boardgame/list/:1" element = { <BoardgameInfo/> } />
+      <Route path="/order-info" element = { <OrderInfo/>} />
     </Routes>
     </BrowserRouter>
   );
