4f8b503bbf7a5df30bee02af574a65ab911a6752
DonghwooCho <whehdgn1001@kookmin.ac.kr>
2022-09-26 02:20:40 +0900

    Add 페이지네이션 임시 이미지 파일 추가

--- src/boardgame/items/Pagenation.js
+++ src/boardgame/items/Pagenation.js
@@ -1,13 +1,7 @@
 import style from "../static/css/pagenation.module.css";
 
 function Pagenation() {
-  return (
-    <div className={style.pagenation}>
-      <div>좌측 버튼</div>
-      <div>--숫자--</div>
-      <div>우측 버튼</div>
-    </div>
-  );
+  return <div className={style.pagenation}></div>;
 }
 
 export default Pagenation;
