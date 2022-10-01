f8f53e67b2c60793cf36058a0f1b9de099137d3a
DonghwooCho <whehdgn1001@kookmin.ac.kr>
2022-10-02 00:22:21 +0900

    Add Error 페이지 파일 추가 Error 페이지 파일 추가와 App.js 라우터 설정 했습니다.

--- src/App.js
+++ src/App.js
@@ -8,21 +8,21 @@ import FindId from "./user/pages/FindId";
 import FindIdResult from "./user/pages/FindIdResult";
 import FindPw from "./user/pages/FindPw";
 import ChangePw from "./user/pages/ChangePw";
-// import PaymentCompleted from "./payment/pages/payment-completed";
+import PaymentCompleted from "./payment/pages/payment-completed";
 import ChangePwResult from "./user/pages/ChangePwResult";
 import BoardgameInfo from "./order/pages/boardgameInfo";
 import BoardgameList from "./boardgame/pages/BoardgameList";
 import Layout from "./layouts/Layout";
 import Main from "./main/pages/Main";
-// import OrderInfo from "./order/pages/order-info";
+import OrderInfo from "./order/pages/order-info";
 import DeliveryAddressList from "./mypage/pages/DeliveryAddressList";
 import DeliveryAddressInsert from "./mypage/pages/DeliveryAddressInsert";
 import DeliveryAddressUpdate from "./mypage/pages/DeliveryAddressUpdate";
 import Mypage from "./layouts/Mypage";
 import EditProfile from "./mypage/pages/EditProfile";
 import Unregist from "./mypage/pages/Unregist";
-import OrderReport from './mypage/pages/orderreport';
-
+import OrderReport from "./mypage/pages/orderreport";
+import Error from "./error/page/Error";
 
 function App() {
   return (
@@ -32,8 +32,8 @@ function App() {
           <Route index element={<Main />} />
           <Route path="/boardgame/list" element={<BoardgameList />} />
           <Route path="/boardgame/list/:1" element={<BoardgameInfo />} />
-          {/* <Route path="/order-info" element={<OrderInfo />} />
-          <Route path="/payment-completed" element={<PaymentCompleted />} /> */}
+          <Route path="/order-info" element={<OrderInfo />} />
+          <Route path="/payment-completed" element={<PaymentCompleted />} />
           <Route path="login" element={<Login />} />
           <Route path="join">
             <Route index element={<Join />} />
@@ -66,8 +66,9 @@ function App() {
               path="delivery-address-update"
               element={<DeliveryAddressUpdate />}
             />
-            <Route path="orderreport" element={<OrderReport/>} />
+            <Route path="orderreport" element={<OrderReport />} />
           </Route>
+          <Route path="/*" element={<Error />} exact />
         </Route>
       </Routes>
     </BrowserRouter>
